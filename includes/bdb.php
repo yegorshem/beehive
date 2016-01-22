0476a795e44c9e7a4c3f1f7d39f6ebdf9168050d
Yegor Shemereko <Yegor Shemereko>
2016-01-21 16:43:13 -0800

    changed directory hierarchy

---
+++ includes/bdb.php
@@ -0,0 +1,22 @@
+<?php
+    /* Yegor Shemereko
+     * Display signup data.
+     */
+
+    $username = "yegor_beeAdmin";
+    $password = "VeryWeakPassword01";
+    $hostname = "localhost";
+
+    try
+    {
+        //Instantiate a database object
+        $db = new PDO("mysql:host=$hostname; dbname=yegor_bee_database", $username, $password);
+        
+        echo 'Connected to database';
+    }
+    catch(PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+        
+?>
