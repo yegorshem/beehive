61b6445aef7726af77930edb1e6663df02021b79
Yegor Shemereko <yshemereko@mail.greenriver.edu>
2016-02-02 17:36:18 -0800

    Hid the password and username

--- includes/bdb.php
+++ includes/bdb.php
@@ -1,22 +1,43 @@
-<?php
-    /* Yegor Shemereko
-     * Display signup data.
-     */
-
-    $username = "yegor_beeAdmin";
-    $password = "VeryWeakPassword01";
-    $hostname = "localhost";
-
-    try
-    {
-        //Instantiate a database object
-        $db = new PDO("mysql:host=$hostname; dbname=yegor_bee_database", $username, $password);
-        
-        echo 'Connected to database';
-    }
-    catch(PDOException $e)
-    {
-        echo $e->getMessage();
-    }
-        
+<?php
+
+    /* Yegor Shemereko
+
+     * Display signup data.
+
+     */
+
+
+
+    $username = "**********";
+
+    $password = "**************";
+
+    $hostname = "localhost";
+
+
+
+    try
+
+    {
+
+        //Instantiate a database object
+
+        $db = new PDO("mysql:host=$hostname; dbname=yegor_bee_database", $username, $password);
+
+        
+
+        echo 'Connected to database';
+
+    }
+
+    catch(PDOException $e)
+
+    {
+
+        echo $e->getMessage();
+
+    }
+
+        
+
 ?>
